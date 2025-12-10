a1481a8ccfe55748fc72f177c0e536f2cad9be37
QMMMS <1595168717@qq.com>
2025-12-10 16:56:17 +0800

    stage_2 and stage_3

--- my_scripts/git.sh
+++ my_scripts/git.sh
@@ -1,3 +1,8 @@
+git config --local user.name "QMMMS"
+git config --local user.email "1595168717@qq.com"
+git config --list
+git add .
+git commit -m "update"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/qinmaosheng.id_rsa
 git push
